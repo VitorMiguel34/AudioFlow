e0a6c59749784b3c521f90d7f100068f9bb9bcd7
Victor <miguelvictor035@gmail.com>
2026-01-19 16:12:57 -0300

    feat: add serializers para playlist e musica

--- back/stream/serializers.py
+++ back/stream/serializers.py
@@ -1,13 +1,30 @@
 from rest_framework import serializers
-from .models import Song,Artist
-
-class SongSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Song
-        fields = ["id","name","duration_time","artist","image","audio"]
-
+from .models import Artist,Playlist,Song
 
 class ArtistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Artist
         fields = ["id","name","image"]
+
+
+class SongSerializer(serializers.ModelSerializer):
+
+    artsist = ArtistSerializer(read_only=True)
+
+    artist_id = serializers.PrimaryKeyRelatedField(
+        queryset=Artist.objects.all(), source='artist', write_only=True
+    )
+
+    class Meta:
+        model = Song
+        fields = ["id","name","duration_time","artist","image","audio","artist_ud"]
+        
+
+class PlaylistSerializer(serializers.ModelSerializer):
+
+    songs = SongSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Playlist
+        fields = ["name,user"]
+
